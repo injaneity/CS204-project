bded866b0c324e7e624fe9f4f9da4327c5fcc579
Joel Lim <110006796+LimJingKaiJoel@users.noreply.github.com>
2024-11-08 22:45:35 +0800

    Chore: refactoring

--- logs_proc2.py
+++ logs_proc2.py
@@ -6,6 +6,8 @@ clusters = []
 cluster_count = 0
 prev_timestamp = None
 TIME_APART_THRESHOLD = 2
+ZERO_ENTRY_THRESHOLD = 6
+MAX_THRESHOLD = 11
 
 # Open and read the text file
 with open('alert_json2.txt', 'r') as f:
@@ -29,12 +31,24 @@ with open('alert_json2.txt', 'r') as f:
         else:
             # Calculate the time difference from the previous timestamp
             time_diff = timestamp - prev_timestamp
-            if time_diff.total_seconds() < TIME_APART_THRESHOLD:
+            seconds_diff = time_diff.total_seconds()
+            
+            if seconds_diff < TIME_APART_THRESHOLD:
                 # Within 2 seconds, continue the current cluster
                 cluster_count += 1
             else:
-                # More than 2 seconds apart, save the current cluster count and start a new cluster
+                # Add the current cluster count to the clusters list
                 clusters.append(cluster_count)
+                
+                # Insert `0` entries based on the time difference
+                if ZERO_ENTRY_THRESHOLD <= seconds_diff < MAX_THRESHOLD:
+                    clusters.append(0)  # One `0` entry if time_diff is between 6 and 11 seconds
+                elif seconds_diff >= MAX_THRESHOLD:
+                    # Calculate the number of `0` entries based on the time difference
+                    zero_entries = int((seconds_diff - TIME_APART_THRESHOLD) // TIME_APART_THRESHOLD)
+                    clusters.extend([0] * zero_entries)
+                
+                # Start a new cluster
                 cluster_count = 1
         
         # Update the previous timestamp
@@ -46,5 +60,5 @@ clusters.append(cluster_count)
 # Output the array of cluster counts
 print(clusters)
 
-# print size of clusters
+# Print size of clusters
 print(len(clusters))
